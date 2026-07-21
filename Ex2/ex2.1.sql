9ccda74c94a49125edb1e28ae7a86d05e0d39c5c
salviasarthak <salviasarthak@gmail.com>
2026-07-21 12:57:30 +0530

    Add files via upload

---
+++ Ex2/ex2.1.sql
@@ -0,0 +1,4 @@
+/* Write a query to output the name of the fruits (f_name) from the table 'fruit' which are not present in the table  inventory(f_name column has the name of the fruits and inv_name has the name of the items in inventory). */
+SELECT f_name FROM fruit
+EXCEPT
+SELECT inv_name FROM inventory;
